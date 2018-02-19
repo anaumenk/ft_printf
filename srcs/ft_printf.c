d8073b14086003bbbb425814de04538b05901826
Alexandra NAUMENKO <anaumenk@e1r7p10.unit.ua>
2018-02-19 22:26:55 +0200

    Sls

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -76,15 +76,84 @@ void unicode(unsigned int x, t_flags *help)
 	}
 }
 
-void for_Sls(va_list args, t_flags *help)
+void for_Sls_cont_three(int i, wchar_t *x, t_flags *help)
 {
-	wchar_t *x;
+	int j;
+
+	j = 0;
+	while (*x && j < i)
+	{
+		unicode(*x, help);
+		j += unilen(*x);
+		x++;
+	}
+	if (help->minus == '-' && i < help->field)
+	{
+		while (i < help->field)
+		{
+			write(1, " ", 1);
+			help->result++;
+			help->field--;
+		}
+	}
+}
+
+void for_Sls_cont_two(int i, t_flags *help, wchar_t *x)
+{
+	if (i < help->field && help->zero == '-' && help->minus != '-')
+	{
+		while (i < help->field)
+		{
+			write(1, " ", 1);
+			help->field--;
+			help->result++;
+		}
+	}
+	if (i < help->field && help->zero == '0' && help->minus != '-')
+	{
+		while (i < help->field)
+		{
+			write(1, "0", 1);
+			help->field--;
+			help->result++;
+		}
+	}
+	for_Sls_cont_three(i, x, help);
+}
+
+void for_Sls_cont_one(t_flags *help, wchar_t *x)
+{
 	int i;
 	int j;
 
-	x = va_arg(args, wchar_t*);
 	i = 0;
 	j = 0;
+	if (help->dot_ex == 1)
+	{
+		while (x[j] && i < help->dot)
+		{
+			i += unilen(x[j]);
+			j++;
+		}
+		if (help->dot != 0 && i != help->dot)
+			i -= unilen(x[j]);
+	}
+	else
+	{
+		while (x[j])
+		{
+			i += unilen(x[j]);
+			j++;
+		}
+	}
+	for_Sls_cont_two(i, help, x);
+}
+
+void for_Sls(va_list args, t_flags *help)
+{
+	wchar_t *x;
+
+	x = va_arg(args, wchar_t*);
 	if (x == NULL && (help->dot_ex != 1 || help->dot >= 6))
 	{
 		write(1, "(null)", 6);
@@ -96,61 +165,7 @@ void for_Sls(va_list args, t_flags *help)
 		help->result += help->dot;
 	}
 	else
-	{
-		if (help->dot_ex == 1)
-		{
-			while (x[j] && i < help->dot)
-			{
-				i += unilen(x[j]);
-				j++;
-			}
-			if (help->dot != 0 && i != help->dot)
-				i -= unilen(x[j]);
-		}
-		else
-		{
-			while (x[j])
-			{
-				i += unilen(*x);
-				j++;
-			}
-		}
-		if (i < help->field && help->zero == '-' && help->minus != '-')
-		{
-			while (i < help->field)
-			{
-				write(1, " ", 1);
-				help->field--;
-				help->result++;
-			}
-		}
-		if (i < help->field && help->zero == '0' && help->minus != '-')
-		{
-			while (i < help->field)
-			{
-				write(1, "0", 1);
-				help->field--;
-				help->result++;
-			}
-		}
-		j = 0;
-		while (*x && j < i)
-		{
-			unicode(*x, help);
-			j += unilen(*x);
-			x++;
-		}
-		if (help->minus == '-' && i < help->field)
-		{
-			while (i < help->field)
-			{
-				write(1, " ", 1);
-				help->result++;
-				help->field--;
-			}
-		}
-	}
-
+		for_Sls_cont_one(help, x);
 }
 
 void for_Ccl_continue(t_flags *help, unsigned int x)
